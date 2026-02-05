621466d527ff0460891a70fb38de99f080a7a195
Anthony Ostia <meximanianace@gmail.com>
2026-02-04 20:51:34 -0800

    set up sound file test

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,25 +3,26 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useAudioPlayer } from "expo-audio";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { Button, useColorScheme } from "react-native";
 import "react-native-reanimated";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useColorScheme } from "react-native";
-import TrackPlayer from "react-native-track-player";
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-TrackPlayer.registerPlaybackService(() => require("./service"));
+const audioSource = require("../assets/hi.mp3");
 
 const RootLayout = () => {
   const colorScheme = useColorScheme();
   const queryClient = new QueryClient();
 
+  const player = useAudioPlayer(audioSource);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <StatusBar style="auto" />
+        <Button title="Play Sound" onPress={() => player.play()} />
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen
