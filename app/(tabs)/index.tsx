e42990b7f4bfb6635cc28e58c0da4f2137d8f40d
Anthony Ostia <meximanianace@gmail.com>
2026-05-26 18:07:02 -0700

    set up color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,8 @@
 import AlbumItem from "@/components/home/album-item";
 import ArtistItem from "@/components/home/artist-item";
+import { ThemeColors } from "@/constants/theme";
+import { useTheme } from "@/contexts/theme-context";
+import { useMemo } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAlbums } from "../../api/queries/albums";
@@ -9,10 +12,10 @@ import { TrackItem } from "../../components/home/track-item";
 import { useAudioPlayerContext } from "../../contexts/audio-player-context";
 
 const HomeScreen = () => {
+  const { colors } = useTheme();
+  const styles = useMemo(() => makeStyles(colors), [colors]);
   const { data: trackData } = useTracks();
-
   const { data: albumData } = useAlbums();
-
   const { data: artistData } = useArtists();
 
   const { loadTrack } = useAudioPlayerContext();
@@ -107,85 +110,54 @@ const HomeScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  albumArt: {
-    width: 160,
-    height: 160,
-    borderRadius: 8,
-  },
-  artistArt: {
-    width: 60,
-    height: 60,
-    borderRadius: 32,
-  },
-  containerContent: {
-    paddingBottom: 48,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  itemsContainer: {
-    display: "flex",
-    flexDirection: "column",
-    gap: 16,
-  },
-  itemsContainerTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    paddingHorizontal: 16,
-  },
-  trackListContainer: {
-    display: "flex",
-    flexDirection: "column",
-    gap: 16,
-    paddingHorizontal: 16,
-  },
-  itemContainerWrapper: {
-    display: "flex",
-    flexDirection: "column",
-    gap: 32,
-  },
-  unavailableText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  albumScrollContent: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 16,
-    paddingHorizontal: 16,
-  },
-  artistScrollContent: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 16,
-    paddingHorizontal: 16,
-  },
-  albumName: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  albumArtist: {
-    fontSize: 14,
-    fontWeight: "500",
-  },
-  artistName: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  artistItem: {
-    display: "flex",
-    backgroundColor: "#f0f0f0",
-    flexDirection: "row",
-    alignItems: "center",
-    padding: 8,
-    gap: 8,
-    width: 240,
-    height: 72,
-    borderRadius: 12,
-  },
-});
+const makeStyles = (colors: ThemeColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+    },
+    containerContent: {
+      paddingBottom: 48,
+    },
+    itemsContainer: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 16,
+    },
+    itemsContainerTitle: {
+      fontSize: 20,
+      fontWeight: "bold",
+      paddingHorizontal: 16,
+      color: colors.primary,
+    },
+    trackListContainer: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 16,
+      paddingHorizontal: 16,
+    },
+    itemContainerWrapper: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 32,
+    },
+    unavailableText: {
+      fontSize: 16,
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    albumScrollContent: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 16,
+      paddingHorizontal: 16,
+    },
+    artistScrollContent: {
+      display: "flex",
+      flexDirection: "row",
+      gap: 16,
+      paddingHorizontal: 16,
+    },
+  });
 
 export default HomeScreen;
