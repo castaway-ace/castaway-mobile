f529af18923a90575d4f1a3d1679c1164a623e16
Anthony Ostia <meximanianace@gmail.com>
2026-05-28 13:14:27 -0700

    Adjust search and settings theme

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,8 +1,14 @@
 import { IconSymbol } from "@/components/ui/icon-symbol";
+import { ThemeColors } from "@/constants/theme";
+import { useTheme } from "@/contexts/theme-context";
+import { useMemo } from "react";
 import { StyleSheet, Text, TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
+  const { colors } = useTheme();
+  const styles = useMemo(() => makeStyles(colors), [colors]);
+
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
       <Text style={styles.title}>Search</Text>
@@ -13,24 +19,26 @@ const Search = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 16,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  search: {
-    padding: 8,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "#000",
-    backgroundColor: "#f0f0f0",
-  },
-});
+const makeStyles = (colors: ThemeColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      padding: 16,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 16,
+      color: colors.primary,
+    },
+    search: {
+      padding: 8,
+      borderRadius: 8,
+      borderWidth: 1,
+      borderColor: colors.surface,
+      backgroundColor: colors.secondary,
+    },
+  });
 
 export default Search;
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,16 @@
+import { ThemeColors } from "@/constants/theme";
 import { useAuth } from "@/contexts/auth-context";
+import { useTheme } from "@/contexts/theme-context";
+import { useMemo } from "react";
 import { Button, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Settings = () => {
   const { logOut } = useAuth();
+
+  const { colors } = useTheme();
+  const styles = useMemo(() => makeStyles(colors), [colors]);
+
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
       <Text style={styles.title}>Settings</Text>
@@ -12,16 +19,19 @@ const Settings = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 16,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
+const makeStyles = (colors: ThemeColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      padding: 16,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 16,
+      color: colors.primary,
+    },
+  });
 
 export default Settings;
